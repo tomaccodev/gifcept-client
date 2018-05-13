54e09ce691cfcc0867d07a0721ca4d8cc879789d
Dani Lupión <danilupion@gmail.com>
2018-05-13 22:35:08 +0200

    Bootstrapping and header UI

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import Header from './header';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/header/index.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+import './Header.css';
+import logo from './images/logo.png';
+
+class Header extends React.Component {
+  public render() {
+    return (
+      <header className="header">
+        <div className="header-wrapper">
+          <div className="header-left">
+            <a href="#" className="header-button header-button-add-gif hide-on-mobile" title="Add new gif">
+              <i className="material-icons">&#xE145;</i>
+            </a>
+            <div className="header-button header-search" title="Search gifs">
+              <i className="material-icons">&#xE8B6;</i>
+              <input type="text" placeholder="Search gifs"/>
+            </div>
+          </div>
+
+          <div className="header-right">
+            <a href="#" className="header-user-name">Carlos</a>
+            <a href="#" className="header-button header-avatar" title="Your Profile"><img
+              src="http://via.placeholder.com/150x150" alt="John Doe"/></a>
+            <a href="#" className="header-button" title="Safe For Work View"><i
+              className="material-icons">&#xE86C;</i></a>
+            <a href="#" className="header-button" title="Your Notifications"><i
+              className="material-icons">&#xE7F5;</i></a>
+          </div>
+          <div className="clearfix"/>
+          <div className="header-logo-wrapper">
+            <img src={logo} alt="Gifcept"/>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
