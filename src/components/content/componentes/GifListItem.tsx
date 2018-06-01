eabc3fb5c7d8a1c10aebe029dfbd9163a4898cde
Dani Lupión <danilupion@gmail.com>
2018-06-02 00:47:45 +0200

    Gifs get played on hover (in the list)

--- src/components/content/componentes/GifListItem.tsx
+++ src/components/content/componentes/GifListItem.tsx
@@ -1,3 +1,5 @@
+import { action, computed, observable } from 'mobx';
+import { observer } from 'mobx-react';
 import * as React from 'react';
 
 import { IGif } from '../../../api/gifs';
@@ -8,42 +10,73 @@ interface IGifListItemProps {
   gif: IGif;
 }
 
-export default ({ gif }: IGifListItemProps) => (
-  <div className="block-item-wrapper">
-    <div className="block-item-top">
-      <span className="block-item-name" title={gif.description}>
-        {gif.description}
-      </span>
-      <i className="material-icons"></i>
-    </div>
-    <a className="block-item-gif-container" style={{ backgroundImage: `url(/${gif.id}.png)` }} />
-    <div className="block-item-bottom">
-      <ul className="block-item-main-actions">
-        <li className="block-item-like">
-          <i className="material-icons"></i>
-          <span className="block-item-like-counter">{gif.likesCount}</span>
-        </li>
-        <li className="block-item-comment">
-          <i className="material-icons"></i>
-          <span className="block-item-comment-counter">{gif.commentsCount}</span>
-        </li>
-        <li className="block-item-recept">
-          <i className="material-icons"></i>
-          <span className="block-item-recept-counter">{gif.sharesCount}</span>
-        </li>
-        <li className="block-item-views">
-          <i className="material-icons"></i>
-          <span className="block-item-views-counter">{gif.viewsCount}</span>
-        </li>
-      </ul>
-      <a href="#" className="block-item-uploader">
-        <img
-          src="http://via.placeholder.com/24x24"
-          className="block-item-uploader-avatar"
-          alt="Periquito de los Palotes"
+@observer
+export default class extends React.Component<IGifListItemProps> {
+  @observable private hovered: boolean = false;
+
+  @computed
+  get imageUrl() {
+    return this.hovered ? `/${this.props.gif.id}.gif` : `/${this.props.gif.id}.png`;
+  }
+
+  public render() {
+    const { gif } = this.props;
+
+    return (
+      <div
+        className="block-item-wrapper"
+        onMouseEnter={this.setHovered}
+        onMouseLeave={this.clearHovered}
+      >
+        <div className="block-item-top">
+          <span className="block-item-name" title={gif.description}>
+            {this.props.gif.description}
+          </span>
+          <i className="material-icons"></i>
+        </div>
+        <a
+          className="block-item-gif-container"
+          style={{ backgroundImage: `url(${this.imageUrl})` }}
         />
-        <span className="block-item-uploader-name">{gif.user.username}</span>
-      </a>
-    </div>
-  </div>
-);
+        <div className="block-item-bottom">
+          <ul className="block-item-main-actions">
+            <li className="block-item-like">
+              <i className="material-icons"></i>
+              <span className="block-item-like-counter">{gif.likesCount}</span>
+            </li>
+            <li className="block-item-comment">
+              <i className="material-icons"></i>
+              <span className="block-item-comment-counter">{gif.commentsCount}</span>
+            </li>
+            <li className="block-item-recept">
+              <i className="material-icons"></i>
+              <span className="block-item-recept-counter">{gif.sharesCount}</span>
+            </li>
+            <li className="block-item-views">
+              <i className="material-icons"></i>
+              <span className="block-item-views-counter">{gif.viewsCount}</span>
+            </li>
+          </ul>
+          <a href="#" className="block-item-uploader">
+            <img
+              src="http://via.placeholder.com/24x24"
+              className="block-item-uploader-avatar"
+              alt="Periquito de los Palotes"
+            />
+            <span className="block-item-uploader-name">{gif.user.username}</span>
+          </a>
+        </div>
+      </div>
+    );
+  }
+
+  @action
+  private clearHovered = () => {
+    this.hovered = false;
+  };
+
+  @action
+  private setHovered = () => {
+    this.hovered = true;
+  };
+}
