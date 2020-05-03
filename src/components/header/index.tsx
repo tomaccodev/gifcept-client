30ba1f58f5d4616dcfb0bff516ec593603a51c2e
Dani Lupión <danilupion@gmail.com>
2020-05-03 23:53:12 +0200

    Solved controller to uncontroller warning

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,3 @@
-import { observer } from 'mobx-react';
 import React, { ChangeEvent, useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -21,86 +20,84 @@ interface IHeaderProps {
   onRatingChange: (rating: Rating) => void;
 }
 
-export default observer(
-  ({
-    loggedUser,
-    currentRating,
-    search,
-    onShowLoginModal,
-    onSearchChange,
-    onShowAddGifModal,
-    onRatingChange,
-  }: IHeaderProps) => {
-    const [searchValue, changeSearchValue] = useState(search);
+export default ({
+  loggedUser,
+  currentRating,
+  search,
+  onShowLoginModal,
+  onSearchChange,
+  onShowAddGifModal,
+  onRatingChange,
+}: IHeaderProps) => {
+  const [searchValue, changeSearchValue] = useState(search || '');
 
-    const ratingSelector = (
-      <RatingSelector currentRating={currentRating} onRatingChange={onRatingChange} />
-    );
+  const ratingSelector = (
+    <RatingSelector currentRating={currentRating} onRatingChange={onRatingChange} />
+  );
 
-    const updateSearchCallback = useCallback(
-      (ev: ChangeEvent<HTMLInputElement>) => {
-        onSearchChange(ev.target.value);
-        changeSearchValue(ev.target.value);
-      },
-      [onSearchChange],
-    );
+  const updateSearchCallback = useCallback(
+    (ev: ChangeEvent<HTMLInputElement>) => {
+      onSearchChange(ev.target.value);
+      changeSearchValue(ev.target.value);
+    },
+    [onSearchChange],
+  );
 
-    const clearSearchCallback = useCallback(() => {
-      onSearchChange('');
-      changeSearchValue('');
-    }, [onSearchChange]);
+  const clearSearchCallback = useCallback(() => {
+    onSearchChange('');
+    changeSearchValue('');
+  }, [onSearchChange]);
 
-    const rightSideButtons = loggedUser ? (
-      <>
-        <span className={styles['header-user-name']}>{loggedUser.username}</span>
-        <HeaderButton
-          title="Your Profile"
-          imgUrl="https://via.placeholder.com/150x150"
-          imgAlt={loggedUser.username}
-        />
-        {ratingSelector}
-        <HeaderButton icon="notifications_none" title="Your Notifications" />
-      </>
-    ) : (
-      <>
-        <HeaderButton icon="face" title="Login" onClick={onShowLoginModal} />
-        {ratingSelector}
-      </>
-    );
+  const rightSideButtons = loggedUser ? (
+    <>
+      <span className={styles['header-user-name']}>{loggedUser.username}</span>
+      <HeaderButton
+        title="Your Profile"
+        imgUrl="https://via.placeholder.com/150x150"
+        imgAlt={loggedUser.username}
+      />
+      {ratingSelector}
+      <HeaderButton icon="notifications_none" title="Your Notifications" />
+    </>
+  ) : (
+    <>
+      <HeaderButton icon="face" title="Login" onClick={onShowLoginModal} />
+      {ratingSelector}
+    </>
+  );
 
-    const addNewGifButton = loggedUser && (
-      <HeaderButton icon="add" title="Add new gif" onClick={onShowAddGifModal} />
-    );
+  const addNewGifButton = loggedUser && (
+    <HeaderButton icon="add" title="Add new gif" onClick={onShowAddGifModal} />
+  );
 
-    return (
-      <div>
-        <header className={styles.header}>
-          <div className={styles['header-wrapper']}>
-            <div className={styles['header-left']}>
-              {addNewGifButton}
-              <div className={styles['header-search']} title="Search gifs">
-                <button onClick={clearSearchCallback}>
-                  <i className="material-icons">{searchValue ? 'clear' : 'search'}</i>
-                </button>
-                <input
-                  type="text"
-                  value={searchValue}
-                  placeholder="Search gifs"
-                  onChange={updateSearchCallback}
-                />
-              </div>
-            </div>
-
-            <div className={styles['header-right']}>{rightSideButtons}</div>
-            <div className={styles['header-logo-wrapper']}>
-              <Link to={'/'}>
-                <img src={logo} alt="Gifcept" />
-              </Link>
+  return (
+    <div>
+      <header className={styles.header}>
+        <div className={styles['header-wrapper']}>
+          <div className={styles['header-left']}>
+            {addNewGifButton}
+            <div className={styles['header-search']} title="Search gifs">
+              <button onClick={clearSearchCallback}>
+                <i className="material-icons">{searchValue ? 'clear' : 'search'}</i>
+              </button>
+              <input
+                type="text"
+                value={searchValue}
+                placeholder="Search gifs"
+                onChange={updateSearchCallback}
+              />
             </div>
           </div>
-        </header>
-        <SubHeader loggedUser={loggedUser} />
-      </div>
-    );
-  },
-);
+
+          <div className={styles['header-right']}>{rightSideButtons}</div>
+          <div className={styles['header-logo-wrapper']}>
+            <Link to={'/'}>
+              <img src={logo} alt="Gifcept" />
+            </Link>
+          </div>
+        </div>
+      </header>
+      <SubHeader loggedUser={loggedUser} />
+    </div>
+  );
+};
